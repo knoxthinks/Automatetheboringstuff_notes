2ab427f9474759aa9482b93b4024172b6a3f96c3
unknown <itsknox@gmail.com>
2020-08-08 02:58:37 -0400

    test

---
+++ ATBS_01.py
@@ -0,0 +1,11 @@
+#This program says hello and asks for my name
+print("Hello World!")
+print("What is your name?") #ask for their name
+myName = input()
+
+print('it is good to meet you,' + myName)
+print('The length of yoyur name is:')
+print(len(myName))
+
+print('what is your age?') #ask for their age
+myAge = input()
